7eb827e762884d9445f0e139c3b8a80f62676bbc
Daan Mathot <daanmathot@gmail.com>
2013-06-11 07:36:27 +0200

    added to make sure modules are installed locally

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+pip=$(which pip) &> /dev/null || pip=$(which pip2) &> /dev/null
+
+[[ -z $pip ]] && echo "ERROR: this install script requires pip: 'https://pypi.python.org/pypi/pip'" && exit 1
+
+$pip install -t ./lib $(cat requirements.txt|tail -$(($(cat requirements.txt|wc -l)-1)))
