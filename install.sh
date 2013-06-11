19c9b4fb74f0c2f92c3a7b76c0f287cceed1eac9
Daan Mathot <daanmathot@gmail.com>
2013-06-11 07:50:09 +0200

    mkdir added

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ pip=$(which pip) &> /dev/null || pip=$(which pip2) &> /dev/null
 
 [[ -z $pip ]] && echo "ERROR: this install script requires pip: 'https://pypi.python.org/pypi/pip'" && exit 1
 
+[[ -d ./lib ]] || mkdir "./lib"
+
 $pip install -t ./lib $(cat .requirements.txt|tail -$(($(cat .requirements.txt|wc -l)-1)))
